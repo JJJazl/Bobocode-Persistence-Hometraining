ef865258223b792df675a3008eb8bc27504fb5ba
Safarov Renat <98067127+JJJazl@users.noreply.github.com>
2023-12-20 09:42:56 +0200

    Initialize "persistence-homework" repository

---
+++ src/main/java/com/example/persistencehomework/PersistenceHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.persistencehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceHomeworkApplication.class, args);
+    }
+
+}
